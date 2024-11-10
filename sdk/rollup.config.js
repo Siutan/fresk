740bac88f9d85ebfd2fd2769fca19d0b4982fefd
mukil cb <mukil@grouptag.app>
2024-11-11 09:56:13 +1100

    tried to change rollup config

--- sdk/rollup.config.js
+++ sdk/rollup.config.js
@@ -2,10 +2,11 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import json from "@rollup/plugin-json";
-//import { terser } from "rollup-plugin-terser";
-//import nodePolyfills from "rollup-plugin-polyfill-node";
+import { terser } from "rollup-plugin-terser";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 import packageJson from "./package.json" assert { type: "json" };
 
+// TODO: optimise dependencies
 const extensions = [".js"];
 
 export default [
@@ -13,39 +14,17 @@ export default [
   {
     input: "src/core/parser.js",
     external: [...Object.keys(packageJson.dependencies)],
-    output: [
-      {
-        file: packageJson.main,
-        format: "esm",
-        exports: "named",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.parser,
-        format: "esm",
-        name: "FreskWebSDK", // Global variable name for the core SDK
-        exports: "named",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.vite,
-        format: "esm",
-        exports: "named",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.shared,
-        format: "esm",
-        exports: "named",
-        sourcemap: true,
-      },
-    ],
+    output: {
+      file: "core/parser.js",
+      format: "esm",
+    },
     plugins: [
+      commonjs({
+        include: /node_modules/,
+      }),
       babel({
-        extensions,
         babelHelpers: "bundled",
-        include: ["src/**/*"],
-        exclude: [/node_modules/, /test/],
+        exclude: "node_modules/**",
       }),
       json(),
       resolve({
@@ -53,9 +32,87 @@ export default [
         rootDir: "./src",
         preferBuiltins: true,
       }),
+      terser(),
+      nodePolyfills(),
+    ],
+  },
+  // Configuration for the core SDK
+  {
+    input: "src/core/index.js",
+    external: [...Object.keys(packageJson.dependencies)],
+    output: {
+      file: "core/index.js",
+      format: "esm",
+      name: "FreskWebSDK", // Global variable name for the core SDK
+    },
+    plugins: [
       commonjs({
         include: /node_modules/,
       }),
+      babel({
+        babelHelpers: "bundled",
+        exclude: "node_modules/**",
+      }),
+      json(),
+      resolve({
+        extensions,
+        rootDir: "./src",
+        preferBuiltins: true,
+      }),
+      terser(),
+      nodePolyfills(),
+    ],
+  },
+  // Configuration for the Vite plugin
+  {
+    input: "src/vite/index.js",
+    external: [...Object.keys(packageJson.dependencies)],
+    output: {
+      file: "vite/index.js",
+      format: "esm",
+      exports: "named",
+    },
+    plugins: [
+      commonjs({
+        include: /node_modules/,
+      }),
+      babel({
+        babelHelpers: "bundled",
+        exclude: "node_modules/**",
+      }),
+      json(),
+      resolve({
+        extensions,
+        rootDir: "./src",
+        preferBuiltins: true,
+      }),
+      terser(),
+    ],
+  },
+  // Configuration for the shared bundler utilities
+  {
+    input: "src/shared/bundler.js",
+    external: [...Object.keys(packageJson.dependencies)],
+    output: {
+      file: "shared/bundler.js",
+      format: "esm",
+      exports: "named",
+    },
+    plugins: [
+      commonjs({
+        include: /node_modules/,
+      }),
+      babel({
+        babelHelpers: "bundled",
+        exclude: "node_modules/**",
+      }),
+      json(),
+      resolve({
+        extensions,
+        rootDir: "./src",
+        preferBuiltins: true,
+      }),
+      terser(),
     ],
   },
 ];
